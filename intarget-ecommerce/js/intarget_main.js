89b74da820745d4fd7a20cdcc819e1489dbf23fa
Gambit <chernozor@yandex.ru>
2016-02-01 16:14:33 +0300

    release

---
+++ intarget-ecommerce/js/intarget_main.js
@@ -0,0 +1,88 @@
+
+(function(d, w, c) {
+    w[c] = {
+        projectId:intarget_vars.project_id
+    };
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "//rt.intarget.ru/loader.js";
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+
+})(document, window, "inTargetInit");
+
+jQuery(document).ready(function(){
+    if (jQuery('body.tax-product_cat').length) {
+
+        (function(w, c) {
+            w[c] = w[c] || [];
+            w[c].push(function(inTarget) {
+
+                inTarget.event('cat-view')
+            });
+        })(window, 'inTargetCallbacks');
+
+
+    }
+})
+
+jQuery(document).ready(function(){
+    if (jQuery('body.single-product').length) {
+
+        (function(w, c) {
+            w[c] = w[c] || [];
+            w[c].push(function(inTarget) {
+
+                inTarget.event('item-view')
+            });
+        })(window, 'inTargetCallbacks');
+
+
+    }
+})
+
+jQuery(document).ready(function(){
+
+    jQuery("button.single_add_to_cart_button").each(function() {
+        var my_funct = " inTarget.event('add-to-cart');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+    })
+
+    jQuery("a.add_to_cart_button").each(function() {
+        var my_funct = " inTarget.event('add-to-cart');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+    })
+
+
+})
+
+
+jQuery(document).ready(function(){
+
+    jQuery("a.remove[data-product_id]").each(function() {
+        var my_funct = " inTarget.event('del-from-cart');";
+        jQuery(this).attr('onclick',my_funct+jQuery(this).attr('onclick'));
+    })
+
+
+})
+
+jQuery(document).ready(function(){
+    if (jQuery('body.woocommerce-order-received').length) {
+
+        (function(w, c) {
+            w[c] = w[c] || [];
+            w[c].push(function(inTarget) {
+
+
+                inTarget.event('user-reg'); inTarget.event('success-order');
+            });
+        })(window, 'inTargetCallbacks');
+
+    }
+})
---
+++ intarget-ecommerce/uninstall.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+$option_name = 'intarget_option_name';
+
+delete_option( $option_name );
